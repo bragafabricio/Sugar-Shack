1c9ecec4044a4f6074509a43a4b68a7523a65975
Fabricio Braga <fabriciobrg@gmail.com>
2022-09-28 21:03:09 -0300

    Increase number of maples in SetupData

--- src/main/java/co/maplr/sugarshack/config/SetupData.java
+++ src/main/java/co/maplr/sugarshack/config/SetupData.java
@@ -25,5 +25,17 @@ public class SetupData implements CommandLineRunner {
         productRepository.save(new MapleSyrupEntity("3", "Red Maple Leaf",
                 "Everyone's favorite Maple Syrup. It's a 50/50 blend of Green and Golden Maple Leaf.",
                 "ImageURL", 15.0, 100, "AMBER"));
+
+        productRepository.save(new MapleSyrupEntity("11", "Greenish Maple Leaf",
+                "This Maple Syrup looks like original, but it's just a mock!",
+                "ImageURL", 9.0, 99, "CLEAR"));
+
+        productRepository.save(new MapleSyrupEntity("22", "Goldish Maple Leaf",
+                "Looks like Golden Syrup, but it is honey actually.",
+                "ImageURL", 19.0, 99, "DARK"));
+
+        productRepository.save(new MapleSyrupEntity("33", "Reddish Maple Leaf",
+                "Watch out! This is Corn Syrup! That's how it should be labelled.",
+                "ImageURL", 4.5, 999, "AMBER"));
     }
 }
