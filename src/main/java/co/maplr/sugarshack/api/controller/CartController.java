c4d2975a2abb323a52d9b8e2336f2487ef83733c
Fabricio Braga <fabriciobrg@gmail.com>
2022-10-02 17:15:36 -0300

    Minor refactoring (Remove @Autowired from constructor because if the class has only one constructor, since Spring 4.3, it is not necessary to explicit write the annotation)

--- src/main/java/co/maplr/sugarshack/api/controller/CartController.java
+++ src/main/java/co/maplr/sugarshack/api/controller/CartController.java
@@ -6,7 +6,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,7 +18,6 @@ public class CartController {
 
     private final CartService cartService;
 
-    @Autowired
     public CartController(CartService cartService) {
         this.cartService = cartService;
     }
--- src/main/java/co/maplr/sugarshack/api/controller/ProductController.java
+++ src/main/java/co/maplr/sugarshack/api/controller/ProductController.java
@@ -10,7 +10,10 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.constraints.NotNull;
 import java.util.List;
@@ -23,7 +26,6 @@ public class ProductController {
     private final CatalogueItemService catalogueItemService;
 
     public ProductController(ProductService productService, CatalogueItemService catalogueItemService) {
-
         this.productService = productService;
         this.catalogueItemService = catalogueItemService;
     }
