40b9fdf0beab4b63f3a1ccf3c36ff945877c4ec0
Fabricio Braga <fabriciobrg@gmail.com>
2022-10-03 21:32:55 -0300

    Include maxQty per order logic in service

--- src/main/java/co/maplr/sugarshack/domain/service/OrderService.java
+++ src/main/java/co/maplr/sugarshack/domain/service/OrderService.java
@@ -1,12 +1,8 @@
 package co.maplr.sugarshack.domain.service;
 
-import co.maplr.sugarshack.api.dto.MapleSyrupDto;
-import co.maplr.sugarshack.api.dto.OrderLineDto;
-import co.maplr.sugarshack.api.dto.OrderValidationResponseDto;
-import co.maplr.sugarshack.domain.entity.MapleSyrupEntity;
 import co.maplr.sugarshack.domain.entity.OrderLineEntity;
 import co.maplr.sugarshack.domain.entity.OrderValidationResponseEntity;
-import org.modelmapper.ModelMapper;
+import co.maplr.sugarshack.domain.entity.ProductEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,49 +14,64 @@ import static java.lang.Math.abs;
 @Service
 public class OrderService {
 
-    private final ModelMapper mapper;
-
     private final ProductService productService;
 
     @Autowired
-    public OrderService(ModelMapper mapper, ProductService productService) {
-        this.mapper = mapper;
+    public OrderService(ProductService productService) {
         this.productService = productService;
     }
 
-    public OrderValidationResponseDto save(List<OrderLineDto> orderLineDtoList) {
+    public OrderValidationResponseEntity saveOrder(List<OrderLineEntity> orderLineEntityList) {
 
         boolean isValid = true;
         List<String> errors = new ArrayList<>();
 
-        for (OrderLineDto order : orderLineDtoList) {
-            if (!isAbleToSaveOrder(order)) {
+        for (OrderLineEntity order : orderLineEntityList) {
+            //isValid = true if, and only if, both "isLessThanMaxQty" and "isStockEnough" are true
+            boolean isOrderLineValid = (isLessThanMaxQty(order, errors) && isStockEnough(order, errors));
+            if (isOrderLineValid) {
+                updateStock(order.getProductId(), order.getQty());
+            } else {
                 isValid = false;
-                int stock = productService.getProductById(order.getProductId()).getStock();
-                errors.add(String.format(
-                        "Not enough of product %s in stock. We have only %d of this left.",
-                        order.getProductId(), stock));
+                //If one orderline is invalid, all orderlines are invalid
             }
         }
 
-        OrderValidationResponseEntity response = new OrderValidationResponseEntity(isValid, errors);
-        return mapper.map(response, OrderValidationResponseDto.class);
+        return new OrderValidationResponseEntity(isValid, errors);
     }
 
-    private boolean isAbleToSaveOrder(OrderLineDto orderLine) {
+    private void updateStock(String productId, int orderQty) {
+        ProductEntity mapleSyrup = productService.getProductById(productId);
+        int newStock = mapleSyrup.getStock() - orderQty;
+        productService.updateProductStock(mapleSyrup, newStock);
+    }
 
-        boolean isOrderValid = true;
-        OrderLineEntity order = mapper.map(orderLine, OrderLineEntity.class);
-        MapleSyrupDto mapleSyrupDto = productService.getProductById(order.getProductId());
-        int newStock = mapleSyrupDto.getStock() - abs(order.getQty());
+    private boolean isLessThanMaxQty(OrderLineEntity order, List<String> errors) {
+        boolean isUnderMaxQty = true;
+        int maxQty = productService.getProductById(order.getProductId()).getMaxQty();
+        if (maxQty < order.getQty()) {
+            isUnderMaxQty = false;
+            errors.add(String.format(
+                    "It looks like somebody's got a sweet tooth! Orders with more than %d are not allowed.", maxQty));
+        }
+        return isUnderMaxQty;
+    }
+
+    private boolean isStockEnough(OrderLineEntity orderLine, List<String> errorsList) {
+
+        boolean isStockHigherThanOrderQty;
+        String productId = orderLine.getProductId();
+        int currentStock = productService.getProductById(productId).getStock();
+        int newStock = currentStock - abs(orderLine.getQty());
 
         if (newStock >= 0) {
-            mapleSyrupDto.setStock(newStock);
-            productService.saveProduct(mapper.map(mapleSyrupDto, MapleSyrupEntity.class));
+            isStockHigherThanOrderQty = true;
         } else {
-            isOrderValid = false;
+            isStockHigherThanOrderQty = false;
+            errorsList.add(String.format(
+                    "Not enough of product %s in stock. We have only %d of this left.", productId, currentStock));
         }
 
-        return isOrderValid;
+        return isStockHigherThanOrderQty;
     }
 }
