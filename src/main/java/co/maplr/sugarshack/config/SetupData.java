5f9158209f0d423b8729b7da32420b9b749d5b49
Fabricio Braga <fabriciobrg@gmail.com>
2022-10-03 21:34:02 -0300

    Chenge setup to reflect change in ProductEntity

--- src/main/java/co/maplr/sugarshack/config/SetupData.java
+++ src/main/java/co/maplr/sugarshack/config/SetupData.java
@@ -1,8 +1,6 @@
 package co.maplr.sugarshack.config;
 
-import co.maplr.sugarshack.domain.entity.CatalogueItemEntity;
-import co.maplr.sugarshack.domain.entity.MapleSyrupEntity;
-import co.maplr.sugarshack.domain.repository.CatalogueItemRepository;
+import co.maplr.sugarshack.domain.entity.ProductEntity;
 import co.maplr.sugarshack.domain.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -13,56 +11,33 @@ public class SetupData implements CommandLineRunner {
     @Autowired
     private ProductRepository productRepository;
 
-    @Autowired
-    private CatalogueItemRepository catalogueItemRepository;
-
-
     @Override
     public void run(String... args) throws Exception {
 
         //productRepository Set Up
-        productRepository.save(new MapleSyrupEntity("1", "Green Maple Leaf",
+        productRepository.save(new ProductEntity("1", "Green Maple Leaf",
                 "This Maple is so pure that it speaks quebecois.",
-                "ImageURL", 10.0, 100, "CLEAR"));
+                "ImageURL", 10.0, 100, 10, "CLEAR"));
 
-        productRepository.save(new MapleSyrupEntity("2", "Golden Maple Leaf",
+        productRepository.save(new ProductEntity("2", "Golden Maple Leaf",
                 "This Maple Syrup is so tasty that the Government made a coin to pay tribute to it.",
-                "ImageURL", 20.0, 100, "DARK"));
+                "ImageURL", 20.0, 100, 10, "DARK"));
 
-        productRepository.save(new MapleSyrupEntity("3", "Red Maple Leaf",
+        productRepository.save(new ProductEntity("3", "Red Maple Leaf",
                 "Everyone's favorite Maple Syrup. It's a 50/50 blend of Green and Golden Maple Leaf.",
-                "ImageURL", 15.0, 100, "AMBER"));
+                "ImageURL", 15.0, 100, 10, "AMBER"));
 
-        productRepository.save(new MapleSyrupEntity("11", "Greenish Maple Leaf",
+        productRepository.save(new ProductEntity("11", "Greenish Maple Leaf",
                 "This Maple Syrup looks like original, but it's just a mock!",
-                "ImageURL", 9.0, 99, "CLEAR"));
+                "ImageURL", 9.0, 99, 10, "CLEAR"));
 
-        productRepository.save(new MapleSyrupEntity("22", "Goldish Maple Leaf",
+        productRepository.save(new ProductEntity("22", "Goldish Maple Leaf",
                 "Looks like Golden Syrup, but it is honey actually.",
-                "ImageURL", 19.0, 99, "DARK"));
+                "ImageURL", 19.0, 99, 10, "DARK"));
 
-        productRepository.save(new MapleSyrupEntity("33", "Reddish Maple Leaf",
+        productRepository.save(new ProductEntity("33", "Reddish Maple Leaf",
                 "Watch out! This is Corn Syrup! That's how it should be labelled.",
-                "ImageURL", 4.5, 999, "AMBER"));
+                "ImageURL", 4.5, 999, 10, "AMBER"));
 
-        //CatalogItemRepository Set Up
-
-        catalogueItemRepository.save(new CatalogueItemEntity("1", "Green Maple Leaf",
-                "ImageURL", 10.0, 100, "CLEAR"));
-
-        catalogueItemRepository.save(new CatalogueItemEntity("2", "Golden Maple Leaf",
-                "ImageURL", 20.0, 100, "DARK"));
-
-        catalogueItemRepository.save(new CatalogueItemEntity("3", "Red Maple Leaf",
-                "ImageURL", 15.0, 100, "AMBER"));
-
-        catalogueItemRepository.save(new CatalogueItemEntity("11", "Greenish Maple Leaf",
-                "ImageURL", 9.0, 99, "CLEAR"));
-
-        catalogueItemRepository.save(new CatalogueItemEntity("22", "Goldish Maple Leaf",
-                "ImageURL", 19.0, 99, "DARK"));
-
-        catalogueItemRepository.save(new CatalogueItemEntity("33", "Reddish Maple Leaf",
-                "ImageURL", 4.5, 999, "AMBER"));
     }
 }
