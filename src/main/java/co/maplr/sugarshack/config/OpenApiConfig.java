1cf9102407a6d1c58670742437304ea19519f661
Fabricio Braga <fabriciobrg@gmail.com>
2022-10-04 21:32:29 -0300

    Add security dependency

--- src/main/java/co/maplr/sugarshack/config/OpenApiConfig.java
+++ src/main/java/co/maplr/sugarshack/config/OpenApiConfig.java
@@ -1,5 +1,7 @@
 package co.maplr.sugarshack.config;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
@@ -7,6 +9,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@SecurityScheme(
+        name = "basicAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "basic"
+)
 public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
